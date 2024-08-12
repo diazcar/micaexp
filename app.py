8efea0fb18cd96ff391462cdcdbbad3366421e4f
Carlos Diaz <carlos.diaz@atmosud.org>
2024-08-12 12:20:47 +0200

    update assets

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app.layout = html.Div(
             dcc.Location(id="url"),
             get_sidebar(),
             get_content(),
-            html.Hr(
-                style={
-                    'borderWidth': "0.5rem",
-                    "width": "100%",
-                    "color": "#17BECF",
-                    }
-                ),
+            # html.Hr(
+            #     style={
+            #         'borderWidth': "0.5rem",
+            #         "width": "100%",
+            #         "color": "#17BECF",
+            #         }
+                # ),
             ]
     )
 
